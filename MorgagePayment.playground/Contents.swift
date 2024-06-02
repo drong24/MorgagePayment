fdd43081f24ddec07cbb297afc1407713ce14de1
drong24 <daisyrong24@gmail.com>
2024-06-02 12:29:38 -0700

    added code to calculate monthly payments

--- MorgagePayment.playground/Contents.swift
+++ MorgagePayment.playground/Contents.swift
@@ -1,4 +1,16 @@
+/*
+ Loan Calculator
+ author: Daisy Rong
+ */
+
 import UIKit
 
-var greeting = "Hello, playground"
-print(greeting)
+func calc(loanAmount: Int, numOfPayments: Int, interestRate: Float) -> Float {
+    let numer = interestRate * pow(1 + interestRate, Float(numOfPayments))
+    let denom = pow(1 + interestRate, Float(numOfPayments)) - 1
+    let result = Float(loanAmount) * numer / denom
+    return result
+}
+
+print(calc(loanAmount: 20000, numOfPayments: 2, interestRate: 0.044 / 12))
+print(calc(loanAmount: 150000, numOfPayments: 30, interestRate: 0.05))
